8ff15d8efa6bd189c3993f2fcdf5794e9fb6d13a
Felix <108411169+felix936@users.noreply.github.com>
2023-09-28 12:48:35 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,15 +4,11 @@ function setup() {
   
   zBereich = createCanvas(windowWidth, windowHeight);
   zBereich.position(0, 0);
-  zBereich.style('z-index', '-1');
+  zBereich.style('z-index', '1');
 }
 
 function draw() {
   noStroke();
-  fill(255, 125, 0, 30);
+  fill(255, 125, 0, 80);
   circle(mouseX, mouseY, 24);
-}
-
-function mouseIsPressed () {
-  background(0);
 }
